b000d77ecec5b370afb079f8f604777bb2ad2ad0
boochow <boochowp@gmail.com>
2021-09-19 00:48:45 +0900

    shorten warning messages

--- prlg2mnlgxd.py
+++ prlg2mnlgxd.py
@@ -276,7 +276,7 @@ if abs(cutoff_eg_int) >= abs(pitch_eg_int) :
     newdata[78:80] = rawdata[80+48:80+50]
     newdata[80] = 0
     if pitch_eg_int > 20:
-        print("Warning: EG is used for cutoff mod(%d), and pitch mod(%d) is ignored" % (cutoff_eg_int, pitch_eg_int))
+        print("Warning: EG is used for cutoff mod(%d), pitch mod(%d) is ignored" % (cutoff_eg_int, pitch_eg_int))
 else:
     newdata[78:80] = rawdata[80+17:80+19]
     if rawdata[80+16] == 1:
@@ -286,7 +286,7 @@ else:
         # target VCO = VCO2
         newdata[80] = 1
     if abs(cutoff_eg_int) > 20:
-        print("Warning: EG is used for pitch mod(%d), and cutoff mod(%d) is ignored" % (pitch_eg_int, cutoff_eg_int))
+        print("Warning: EG is used for pitch mod(%d), cutoff mod(%d) is ignored" % (pitch_eg_int, cutoff_eg_int))
 # LFO
 newdata[81] = rawdata[80+70]
 if rawdata[80+71] == 0:
